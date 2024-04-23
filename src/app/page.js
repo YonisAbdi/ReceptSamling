dde3ee6285122c46b1709d612b5fe59d8117cd22
Mexoor <73921858+Mexoor@users.noreply.github.com>
2024-04-23 10:41:48 +0200

    IndexedDB

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import Recipes from "./components/recepies";
+import createDatabase from "./indexDB/db";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() =>{
+    async function doDBOperations(){
+      const db = await createDatabase();
+
+      await db.add('books',{title: 'Harry Potter', author: 'JK Rowling'});
+
+      const books = await db.getAll('books');
+      console.log(books);
+    }
+
+    doDBOperations();
+  }, []);
+
   return (
     <main className={styles.main}>
       <Recipes />
