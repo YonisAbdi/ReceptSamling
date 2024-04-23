3d2b51a1e13dc0463ba6236c4cb2c664b069af24
Yonis Abdi <yonisabdi.jobb@gmail.com>
2024-04-23 18:41:25 +0200

    update

--- utils/storage.js
+++
@@ -1,9 +0,0 @@
-// utils/storage.js
-export const saveToLocalStorage = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  };
-  
-  export const getFromLocalStorage = (key) => {
-    return JSON.parse(localStorage.getItem(key));
-  };
-  
