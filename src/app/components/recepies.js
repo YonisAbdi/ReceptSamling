2f74c7e7fe4fb8ab21ac630b4534942b0de6e0b0
Mexoor <73921858+Mexoor@users.noreply.github.com>
2024-04-25 07:07:09 +0200

    Visar stjärna och sparar i localStorage

--- src/app/components/recepies.js
+++ src/app/components/recepies.js
@@ -3,7 +3,7 @@
 // components/Recipes.js
 
 import createDatabase from '../indexDB/db';
-
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 
@@ -37,6 +37,14 @@ function Recipes() {
     const handleSearch = (e) => {
         setSearchQuery(e.target.value);
     };
+    
+    //Function to store favorite in localStorage
+    const toggleFavorite = (index) => {
+        const updatedRecipes = [...recepies];
+        updatedRecipes[index].isFavorite = !updatedRecipes[index].isFavorite;
+        setRecepies(updatedRecipes);
+        localStorage.setItem('recepies', JSON.stringify(updatedRecipes));
+    };
 
     return (
         <div>
@@ -50,9 +58,20 @@ function Recipes() {
             {filteredRecepies.length === 0 ? (
                 <p>No recipes found.</p>
             ) : (
-                filteredRecepies.map((recipe) => (
+                filteredRecepies.map((recipe, index) => (
                     <div key={recipe.id}>
-                        <h2>{recipe.name}</h2>
+                        <h2>{recipe.name} 
+                        {/* The click event to change favorite*/}
+                            <a onClick={() => toggleFavorite(index)}>
+                                <Image 
+                                    id={`favorite-${index}`} 
+                                    src={recipe.isFavorite ? "./star-fill.svg" : "./star.svg"} 
+                                    width={32} 
+                                    height={32} 
+                                    alt='starIcon'>
+                                </Image>
+                            </a>                                    
+                        </h2>
                         <h3>Ingredients:</h3>
                         <ul>
                             {recipe.ingredients.map((ingredient, index) => (
